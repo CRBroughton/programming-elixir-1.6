c46590e484757afe139ba2051290e4f9b87c69ee
CRBroughton <crbroughton@posteo.uk>
2022-10-15 15:48:39 +0100

    add with-scope example

---
+++ 02-conventional-programming/04-elixir-basics/with-scope.exs
@@ -0,0 +1,15 @@
+content = "Now is the time"
+
+lp =
+  with {:ok, file} = File.open("/etc/passwd"),
+       # note: same name as above
+      content = IO.read(file, :all),
+      :ok = File.close(file),
+      [_, uid, gid] = Regex.run(~r/^lp:.*?:(\d+):(\d+)/m, content)
+  do
+    "Group: #{gid}, User: #{uid}"
+  end
+
+IO.puts(lp)
+IO.puts(content)
+# => Group: 26, User: 26
